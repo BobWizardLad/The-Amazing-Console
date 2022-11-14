4071b4aea3d795187e8675124b7694ada5f1161d
Suda-C <98287934+Suda-C@users.noreply.github.com>
2022-11-14 12:57:36 -0600

    converted to level function

--- secret.py
+++ secret.py
@@ -1,25 +1,23 @@
 
 def pegasus():
 
-    level1 = True
-
-    print("\n*******************************************************************"+
+    level_system = True
+    print("\n*******************************************************************" +
           "\nWe are team PEGASUS, commissioned by Aniket, Austin, and Chandra." +
           "\nYour goal is to decrypt this message, and find what the secret is.")
 
-    original_message = "This is a top-secret message"
-    encrypted_message = "Uijt jt b upq-tfdsfu nfttbhf"
+    def new_level(original_message, encrypted_message):
+        print("\n\nThis is the encrypted message: " + encrypted_message)
 
-    print("\n\nThis is the encrypted message: " + encrypted_message)
+        decrypted_message = input("\n\nDecrypt this message. [Caesar Cipher]\n-> ")
 
-    decrypted_message = input("\n\nDecrypt this message. [Caesar Cipher]\n-> ")
-
-<<<<<<< HEAD
-    while lvl1:
         if decrypted_message == original_message:
-            print("Correct!"+
-                "\n*******************************************************************")
-            lvl1 = False
+            print("Correct!" +
+                  "\n*******************************************************************")
+
         else:
             print("Hmm, something is wrong. Please check your answer..." +
-                "\n*******************************************************************")
+                  "\n*******************************************************************")
+    
+    while True:
+        new_level("This is a top-secret message", "Uijt jt b upq-tfdsfu nfttbhf")
