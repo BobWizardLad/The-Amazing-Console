255ac941920b31a8af226aa76681419d610a2b2f
RohanS1 <53277674+RohanS1@users.noreply.github.com>
2022-11-14 13:12:48 -0600

    imported libraries
    imported flagpy art and countryinfo

--- Global.py
+++ Global.py
@@ -1,28 +1,42 @@
+# Import libraries
 import art
 import flagpy as fp
-
 from countryinfo import CountryInfo
 
-
-art.tprint("Welcome  to  Global")
+# Tprint for print text
+art.tprint("Welcome  to  Global!")
 
 # Code for user-input
 print("Welcome to global! \n Pick a Country")
 user_country = input("My country is: ")
 
+# Setting up and defining correct answers
 art.tprint(user_country)
 img = fp.display(user_country)
-
 country = CountryInfo(user_country)
 answer = country.capital()
+answerTwo = country.region()
 
+# Question One
 print("What is the capital of " + user_country + "?")
+print()
 user_guess = input("Answer: ")
 
+# Determining if answer is correct or false
 if answer == user_guess:
-  print("Great job, you got it right!")
+  print("Great job, you got it right! 😃")
 else:
-    print("You have done " + user_country + " wrong.")
+    print("Incorrect!")
+
+# Question Two
+print()
+print("What continent is " + user_country + " in?")
+user_guess_two = input("Answer: ")
+
+if user_guess_two == answerTwo:
+    print("Correct! 😃")
+else:
+    print("Incorrect!")
 
 
 
