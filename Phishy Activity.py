f8168027b598141bdcd3569b50e98fb6c33ca369
MarsIgloo <wyatt@marsiglio.com>
2022-11-14 13:15:36 -0600

    Edited other email prompts to fit needs of the specific emails

--- Phishy Activity.py
+++ Phishy Activity.py
@@ -11,6 +11,49 @@ def phishy():
     score = 0
 
 
+    #Email 3 - phising
+    def email3(score):
+
+        print("Phising Email here")
+        print("")
+
+        userInput = input("Is this phising? Y or N :  ")
+
+        if userInput == 'y' or 'Y':
+            score += 1
+            print("Good catch! Your final score is: " + score)
+
+        elif userInput == 'n' or 'N':
+            print("Good try, but this email is phising")
+
+        else:
+            print("That wasn't an option, try tht again")
+            email3(score)
+
+    # Email 2 - safe
+    def email2(score):
+        print("Safe Email here")
+        print("")
+
+        userInput = input("Is this phising? Y or N :  ")
+
+        if userInput == 'y' or 'Y':
+            print("Good try, but this email is safe")
+
+        elif userInput == 'n' or 'N':
+            score += 1
+            print("Good catch! Your new score is: " + score)
+
+            print("")
+            print("Here comes the next email")
+            print("")
+            email3(score)
+
+        else:
+            print("That wasn't an option, try tht again")
+            email2(score)
+
+    #Email 1 - safe
     def email1(score):
         print("Phising Email here")
         print("")
@@ -21,17 +64,16 @@ def phishy():
             score += 1
             print("Good catch! Your new score is: " + score)
 
+            print("")
+            print("Here comes the next email")
+            print("")
+            email2(score)
         elif userInput == 'n' or 'N':
             print("Good try, but this email is phising")
 
         else:
             print("That wasn't an option, try tht again")
-            email1()
-
-    #Email 2 - safe
-
-
-    #Email 3 - phising
+            email1(score)
 
     #Funtion call
     email1(score)
