eb11a41bfb759fb193e5c92dd18253c6f35b482a
ludaChris2023 <117676376+ludaChris2023@users.noreply.github.com>
2022-11-14 13:48:07 -0600

    Debugged Win
    Change cast as tuple to cast as set, to allow program to check if all of the items were equal in the spin.

--- lucky.py
+++ lucky.py
@@ -1,37 +1,41 @@
-def slotMachine(balance):
-    import random
-    wordList = ["Banana", "Apples", "Grapes", "7up", "Sheesh", "LIL JOSH", "Dr. Chandra", "Yung Alton", "LudaChris",
-                "Snickers"]
-    n = 0
-    slotList = []
-    while (n < 3):
-        x = random.randint(1, len(wordList))
-        slotList += [wordList[x]]
-        n += 1
-    nonRepeatSlotList = (slotList)
-    if len(nonRepeatSlotList) == 1:
-        print("YOU WIN")
-        balance += 1000
-    else:
-        print("Did you know that 99% of gamblers quit before their first big win?")
-        balance -= 250
-    print(slotList)
-def teamLuck(balance):
+
+
+
+def teamLuck():
+    balance = 1000
     print("Welcome to the CASINO")
     print("Lets go get some MONEY$$$. SHEEESH")
     print("Your starting balance is", balance)
     print("Your goal is to get to 2,000")
 
 
-    print("Slotmachine? (Y/N)")
+    print("Slotmachine? (y/n)")
     decision = input()
 
-    while (decision == "Y"):
-        slotMachine(balance)
-        print("Would you like to keep going?")
-        decision = input()
-    if (decision == "N"):
-        print("You are not smart. You could've won big.")
+    while (decision == "y"):
+        import random
+        wordList = ["Banana", "Apples", "Grapes", "7up", "Sheesh", "LIL JOSH", "Dr. Chandra", "Yung Alton", "LudaChris", "Snickers"]
+        n = 0
+        slotList = []
+        while (n < 3):
+            x = random.randint(0, len(wordList) - 1)
+            slotList += [wordList[x]]
+            n += 1
+        nonRepeatSlotList = set(slotList)
 
-teamLuck(1000)
+        if len(nonRepeatSlotList) == 1:
+            print("YOU WIN")
+            balance += 1000
+        else:
+            print("Did you know that 99% of gamblers quit before their first big win?")
+            balance -= 250
+        print(slotList)
+        print("Would you like to keep going?")
+        print(balance)
+        decision = input()
+    if (decision == "n"):
+        print("You are not smart. You could've won big.")
+        print(balance)
+
+teamLuck()
 
