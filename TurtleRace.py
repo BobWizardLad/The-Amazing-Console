35330f6dbae2f627e04d9c655f83dbc2c9bd7535
iamnotaperson <rawlingse@bentonvillek12.org>
2022-11-14 13:13:05 -0600

    Update TurtleRace.py
    First Turtle

--- TurtleRace.py
+++ TurtleRace.py
@@ -2,4 +2,28 @@ from turtle import *
 from random import randit
 
 def Race():
-    
+
+    speed(0)
+    penup()
+    goto(-140, 140)
+
+    for step in range(15):
+        write(step, align='center')
+        right(90)
+        for num in range(8):
+            penup()
+            forward(10)
+            pendown()
+            forward(10)
+        penup()
+        backward(160)
+        left(90)
+        forward(20)
+
+    ada = Turtle()
+    ada.color('red')
+    ada.shape('turtle')
+
+    ada.penup()
+    ada.goto(-160, 100)
+    ada.pendown()
