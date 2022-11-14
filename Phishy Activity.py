d6bd1148af60647ead1fc5ec7c290df8931d5891
MarsIgloo <wyatt@marsiglio.com>
2022-11-14 13:36:02 -0600

    Fixed some UI errors

--- Phishy Activity.py
+++ Phishy Activity.py
@@ -1,29 +1,31 @@
 def phishy():
+    print("---------------------------------------------------------")
     print("You Have chosen the fishing tab... oh. I mean Phising tab")
-    print("Good luck")
     print("")
     print("This task includes deciding weather an email that is given to you phising or safe.")
-    print("")
-    print("")
+    print("---------------------------------------------------------")
     print("The first email is coming up, DON'T GET PHISHED")
+    print("")
     print("We'll be keeping score. Current Score: 0")
+    print("---------------------------------------------------------")
+
 
     score = 0
 
 
     #Email 3 - phising
     def email3(score):
-
         print("Phising Email here")
         print("")
+        print("---------------------------------------------------------")
 
         userInput = input("Is this phising? Y or N :  ")
 
-        if userInput == 'y' or 'Y':
+        if userInput == "y" or userInput == "Y":
             score += 1
-            print("Good catch! Your final score is: " + score)
+            print("Good catch! Your final score is: " + str(score))
 
-        elif userInput == 'n' or 'N':
+        elif userInput == "n" or userInput == "N":
             print("Good try, but this email is phising")
 
         else:
@@ -34,15 +36,17 @@ def phishy():
     def email2(score):
         print("Safe Email here")
         print("")
+        print("---------------------------------------------------------")
 
         userInput = input("Is this phising? Y or N :  ")
 
-        if userInput == 'y' or 'Y':
+        if userInput == 'y' or userInput == 'Y':
             print("Good try, but this email is safe")
+            email3(score)
 
-        elif userInput == 'n' or 'N':
+        elif userInput == 'n' or userInput == 'N':
             score += 1
-            print("Good catch! Your new score is: " + score)
+            print("Good catch! Your new score is: " + str(score))
 
             print("")
             print("Here comes the next email")
@@ -57,19 +61,21 @@ def phishy():
     def email1(score):
         print("Phising Email here")
         print("")
+        print("---------------------------------------------------------")
 
         userInput = input("Is this phising? Y or N :  ")
 
-        if userInput == 'y' or 'Y':
+        if userInput == "y" or userInput == "Y":
             score += 1
-            print("Good catch! Your new score is: " + score)
-
+            print("---------------------------------------------------------")
+            print("Good catch! Your new score is: " + str(score))
             print("")
             print("Here comes the next email")
-            print("")
+            print("---------------------------------------------------------")
             email2(score)
-        elif userInput == 'n' or 'N':
+        elif userInput == "n" or userInput == "N":
             print("Good try, but this email is phising")
+            email2(score)
 
         else:
             print("That wasn't an option, try tht again")
@@ -78,6 +84,6 @@ def phishy():
     #Funtion call
     email1(score)
 
-    pass
+
 
 phishy()
