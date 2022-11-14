e87dc166cd515b8542e85ac266aefb90b6bc7034
Austin Dolan <austindolan.online@gmail.com>
2022-11-14 12:34:01 -0600

    Added First Decryption Challenge

--- secret.py
+++ secret.py
@@ -1,6 +1,19 @@
 
-
 def pegasus():
     print("We are team PEGASUS, commissioned by Aniket, Austin, and Chandra." +
           "\nYour goal is to decrypt this message, and find what the secret is.")
-    print("\n\n***Your Message: jihkd uibd7 kjnedk jk3")
+
+    original_message = "This is a top-secret message"
+    encrypted_message = "Uijt jt b upq-tfdsfu nfttbhf"
+
+    print("\n\nThis is the encrypted message: " + encrypted_message)
+
+    decrypted_message = input("\n\nDecrypt this message. [Caesar Cipher] -> ")
+
+    if decrypted_message == original_message:
+        print("Correct!")
+    else:
+        print("Hmm, something is wrong. Please check your answer...")
+
+
+
